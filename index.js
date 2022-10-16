8a508664faf111cb05804db72b2e8d041226d14b
DivyarajsinhRana <divyarajsinh.rana@archesoftronix.com>
2022-10-16 17:55:48 +0530

    registe user done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose')
 require('dotenv').config();
@@ -6,6 +7,7 @@ const routes = require('./routes/routes');
 
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use('/api', routes);
